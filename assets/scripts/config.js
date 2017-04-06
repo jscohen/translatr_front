520c0c5023b3ab8fb87c2c2b4038be1e2d80fa12
jscohen <jon.cohen08@gmail.com>
2017-04-06 09:11:33 -0400

    Updated config.js to show update provided by Chris

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    // production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
   }
 }
 
